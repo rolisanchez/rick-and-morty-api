f4605e16b3ad2cccc1da0206303a95aeb534bdbe
Victor Rolando Sanchez Jara <roli.s91@gmail.com>
2018-10-06 17:31:48 +0800

    Added AppIcon and Launchscreen Image

---
+++ RickAndMortyShowcase/RickAndMortyShowcase/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RickAndMortyShowcase
+//
+//  Created by Victor Rolando Sanchez Jara on 6/10/18.
+//  Copyright © 2018 vrsj. All rights reserved.
+//
+
+import Foundation
+
+struct RickAndMortyConstants {
+    struct colors {
+        static let backgroundGreen = "34d878"
+    }
+}
